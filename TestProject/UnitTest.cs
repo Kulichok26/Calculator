f21f38ef15fd7a8511c57bda2af422f80641a511
Kulichok26 <katyusha.kulikova.2005@mail.ru>
2025-12-13 16:55:26 +0300

    add all commit

--- TestProject/UnitTest.cs
+++ TestProject/UnitTest.cs
@@ -14,5 +14,42 @@ namespace TestProject
             string actual = Calculate.main(a, b, "+").ToString();
             Assert.Equal(expected, actual);
         }
+        [Fact]
+        public void Test2()
+        {
+            string a = "3";
+            string b = "4";
+            string expected = "-1";
+            string actual = Calculate.main(a, b, "-").ToString();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test3()
+        {
+            string a = "3";
+            string b = "4";
+            string expected = "12";
+            string actual = Calculate.main(a, b, "*").ToString();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test4()
+        {
+            string a = "4";
+            string b = "4";
+            string expected = "1";
+            string actual = Calculate.main(a, b, "/").ToString();
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test5()
+        {
+            string a = "5";
+            string b = "4";
+            string expected = "1";
+            string actual = Calculate.main(a, b, "r").ToString();
+            Assert.Equal(expected, actual);
+        }
+
     }
 }
